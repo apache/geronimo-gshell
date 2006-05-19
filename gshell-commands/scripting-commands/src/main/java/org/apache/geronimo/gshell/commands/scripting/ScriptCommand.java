7f00230d4b30fc07ba62acadea1facf25c4d1874
Jason Dillon <jdillon@apache.org>
2006-05-19 23:04:27 +0000

    An initial stab at scripting support
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/gshell/trunk@407929 13f79535-47bb-0310-9956-ffa450edef68

---
+++ gshell-commands/scripting-commands/src/main/java/org/apache/geronimo/gshell/commands/scripting/ScriptCommand.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.geronimo.gshell.commands.scripting;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.lang.StringUtils;
+
+import org.apache.geronimo.gshell.command.Command;
+import org.apache.geronimo.gshell.command.CommandSupport;
+import org.apache.geronimo.gshell.console.IO;
+import org.apache.geronimo.gshell.console.JLineConsole;
+import org.apache.bsf.BSFManager;
+import org.apache.bsf.BSFEngine;
+
+/**
+ * Provides generic scripting language integration via <a href="http://http://jakarta.apache.org/bsf">BSF</a>.
+ *
+ * @version $Id: CatCommand.java 399599 2006-05-04 08:13:57Z jdillon $
+ */
+public class ScriptCommand
+    extends CommandSupport
+{
+    private String language;
+
+    private boolean interactive = false;
+
+    private String expression
+            ;
+
+    public ScriptCommand() {
+        super("script");
+    }
+
+    protected int doExecute(final String[] args) throws Exception {
+        assert args != null;
+
+        //
+        // TODO: Optimize, move common code to CommandSupport
+        //
+
+        IO io = getIO();
+
+        Options options = new Options();
+
+        options.addOption(OptionBuilder.withLongOpt("help")
+            .withDescription("Display this help message")
+            .create('h'));
+
+        options.addOption(OptionBuilder.withLongOpt("lang")
+            .withDescription("Specify the scripting language")
+            .hasArg()
+            .create('l'));
+
+        options.addOption(OptionBuilder.withLongOpt("expression")
+            .withDescription("Evaluate the given expression")
+            .hasArg()
+            .create('e'));
+
+        options.addOption(OptionBuilder.withLongOpt("interactive")
+            .withDescription("Run interactive mode")
+            .create('i'));
+
+        CommandLineParser parser = new PosixParser();
+        CommandLine line = parser.parse(options, args);
+
+        if (line.hasOption('h')) {
+            io.out.println(getName() + " -- scripting language integration");
+            io.out.println();
+
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(
+                io.out,
+                80, // width (FIXME: Should pull from gshell.columns variable)
+                getName() + " [options]",
+                "",
+                options,
+                4, // left pad
+                4, // desc pad
+                "",
+                false); // auto usage
+
+            io.out.println();
+
+            return Command.SUCCESS;
+        }
+
+        if (line.hasOption('l')) {
+            this.language = line.getOptionValue('l');
+        }
+
+        if (line.hasOption('e')) {
+            this.expression = line.getOptionValue('e');
+        }
+
+        if (line.hasOption('i')) {
+            this.interactive = true;
+        }
+
+        //
+        // TODO: When given a file/url, try to figure out language from ext if language not given
+        //
+
+        if (language == null) {
+            throw new RuntimeException("Must specify a language");
+        }
+
+        if (!BSFManager.isLanguageRegistered(language)) {
+            throw new RuntimeException("Language is not registered: " + language);
+        }
+
+        BSFManager manager = new BSFManager();
+        BSFEngine engine = manager.loadScriptingEngine(language);
+
+        if (this.expression != null) {
+            log.info("Evaluating expression: " + expression);
+
+            Object obj = engine.eval("<unknown>", 1, 1, expression);
+
+            log.info("Expression result: " + obj);
+        }
+
+        if (this.interactive) {
+            String prompt = "script:" + language + ">";
+            InteractiveInterpreter interp = new InteractiveInterpreter(new JLineConsole(getIO()), engine, prompt);
+            interp.run();
+        }
+
+        return Command.SUCCESS;
+    }
+}
