f254b249625a0c61eea954bf2307de8094192a4c
Jason Dillon <jdillon@apache.org>
2006-06-01 14:29:12 +0000

    Added crude topic support to help command
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/gshell/trunk@410868 13f79535-47bb-0310-9956-ffa450edef68

--- gshell-commands/standard-commands/src/main/java/org/apache/geronimo/gshell/commands/standard/HelpCommand.java
+++ gshell-commands/standard-commands/src/main/java/org/apache/geronimo/gshell/commands/standard/HelpCommand.java
@@ -29,7 +29,7 @@ import org.apache.geronimo.gshell.command.CommandManager;
 import org.apache.geronimo.gshell.console.IO;
 
 /**
- * Display command help
+ * Display help
  *
  * @version $Id$
  */
@@ -58,15 +58,30 @@ public class HelpCommand
         CommandLineParser parser = new PosixParser();
         CommandLine line = parser.parse(options, args);
 
-        if (line.hasOption('h')) {
-            io.out.println(getName() + " -- display gshell help");
+        boolean usage = false;
+        String topic = null;
+
+        String[] _args = line.getArgs();
+        if (_args.length > 1) {
+            io.err.println("Too many arguments: " + _args);
+            return Command.FAILURE;
+        }
+        else if (_args.length == 1) {
+            topic = _args[0];
+        }
+        else {
+            usage = true;
+        }
+
+        if (usage || line.hasOption('h')) {
+            io.out.println(getName() + " -- display help");
             io.out.println();
 
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp(
                 io.out,
                 80, // width (FIXME: Should pull from gshell.columns variable)
-                getName() + " [options]",
+                getName() + " [options] [topic]",
                 "",
                 options,
                 4, // left pad
@@ -75,22 +90,38 @@ public class HelpCommand
                 false); // auto usage
 
             io.out.println();
+            io.out.println("For a list of topics try: help topics");
+            io.out.println();
 
             return Command.SUCCESS;
         }
 
         //
-        // HACK: Need to DI this guy, but for now this will work
-        //
-        
-        CommandManager manager = new CommandManager();
-
-        //
-        // HACK: For now just list all know commands
+        // TODO: Reuse our command bits...
         //
 
-        for (String name : manager.commandNames()) {
-            io.out.println(name);
+        if (topic.equals("topics")) {
+            io.out.println("Available topics:");
+            io.out.println("  topics");
+            io.out.println("  commands");
+        }
+        if (topic.equals("commands")) {
+            //
+            // HACK: Need to DI this guy, but for now this will work
+            //
+
+            CommandManager manager = new CommandManager();
+
+            io.out.println("Available commands:");
+
+            //
+            // HACK: For now just list all know commands
+            //
+
+            for (String name : manager.commandNames()) {
+                io.out.print("  ");
+                io.out.println(name);
+            }
         }
 
         return Command.SUCCESS;
