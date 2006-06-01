33278c03eb4c86cb00dfc256eaec70cc5641a135
Jason Dillon <jdillon@apache.org>
2006-06-01 14:15:51 +0000

    Support URLs
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/gshell/trunk@410867 13f79535-47bb-0310-9956-ffa450edef68

--- gshell-commands/standard-commands/src/main/java/org/apache/geronimo/gshell/commands/standard/CatCommand.java
+++ gshell-commands/standard-commands/src/main/java/org/apache/geronimo/gshell/commands/standard/CatCommand.java
@@ -31,9 +31,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.MalformedURLException;
 
 /**
- * Concatenate and print files.
+ * Concatenate and print files and/or URLs.
  *
  * @version $Id$
  */
@@ -41,66 +44,66 @@ public class CatCommand
     extends CommandSupport
 {
     private boolean displayLineNumbers = false;
-    
+
     public CatCommand() {
         super("cat");
     }
-    
+
     protected int doExecute(final String[] args) throws Exception {
         assert args != null;
-        
+
         //
         // TODO: Optimize, move common code to CommandSupport
         //
-        
+
         IO io = getIO();
-        
+
         Options options = new Options();
-        
+
         options.addOption(OptionBuilder.withLongOpt("help")
             .withDescription("Display this help message")
             .create('h'));
-        
+
         options.addOption(OptionBuilder
             .withDescription("Number the output lines, starting at 1")
             .create('n'));
-        
+
         CommandLineParser parser = new PosixParser();
         CommandLine line = parser.parse(options, args);
-        
+
         if (line.hasOption('h')) {
-            io.out.println(getName() + " -- concatenate and print files");
+            io.out.println(getName() + " -- concatenate and print files and/or URLs");
             io.out.println();
-            
+
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp(
                 io.out,
                 80, // width (FIXME: Should pull from gshell.columns variable)
-                getName() + " [options] [file ...]",
+                getName() + " [options] [<file|url> ...]",
                 "",
                 options,
                 4, // left pad
                 4, // desc pad
                 "",
                 false); // auto usage
-            
+
             io.out.println();
-            
+
             return Command.SUCCESS;
         }
-        
+
         if (line.hasOption('n')) {
             displayLineNumbers = true;
         }
-        
+
         cat(line.getArgs());
-        
+
         return Command.SUCCESS;
     }
-    
+
     private void cat(final String[] files) throws IOException {
         assert files != null;
-        
+
         IO io = getIO();
 
         for (String filename : files) {
@@ -113,10 +116,20 @@ public class CatCommand
             if (files.length == 1 && "-".equals(files[0])) {
                 log.info("Printing STDIN");
                 reader = new BufferedReader(io.in);
-            } else {
-                File file = new File(filename);
-                log.info("Printing file: " + file);
-                reader = new BufferedReader(new FileReader(file));
+            }
+            else {
+                // First try a URL
+                try {
+                    URL url = new URL(filename);
+                    log.info("Printing URL: " + url);
+                    reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                }
+                catch (MalformedURLException ignore) {
+                    // They try a file
+                    File file = new File(filename);
+                    log.info("Printing file: " + file);
+                    reader = new BufferedReader(new FileReader(file));
+                }
             }
 
             String line;
