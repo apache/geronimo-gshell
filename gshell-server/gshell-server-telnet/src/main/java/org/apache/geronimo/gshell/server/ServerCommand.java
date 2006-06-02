d7ffa7d0e6098f6fa3af3c43a603ef56f9a867e4
Jason Dillon <jdillon@apache.org>
2006-06-02 03:37:18 +0000

    Moving things around a bit
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/gshell/trunk@411022 13f79535-47bb-0310-9956-ffa450edef68

---
+++ gshell-server/gshell-server-telnet/src/main/java/org/apache/geronimo/gshell/server/ServerCommand.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.geronimo.gshell.server;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
+import org.apache.geronimo.gshell.command.Command;
+import org.apache.geronimo.gshell.command.CommandSupport;
+import org.apache.geronimo.gshell.console.IO;
+import org.apache.geronimo.gshell.server.SocketServerDaemon.SocketHandler;
+
+import java.net.Socket;
+
+/**
+ * Starts a GShell server.
+ *
+ * @version $Id$
+ */
+public class ServerCommand
+    extends CommandSupport
+{
+    private int port = 5057;
+
+    public ServerCommand() {
+        super("server");
+    }
+
+    protected int doExecute(final String[] args) throws Exception {
+        assert args != null;
+
+        //
+        // TODO: Optimize, move common code to CommandSupport
+        //
+
+        IO io = getIO();
+
+        Options options = new Options();
+
+        options.addOption(OptionBuilder.withLongOpt("help")
+            .withDescription("Display this help message")
+            .create('h'));
+
+        options.addOption(OptionBuilder.withLongOpt("port")
+            .withDescription("Use a specified port number")
+            .hasArg()
+            .create('p'));
+
+        CommandLineParser parser = new PosixParser();
+        CommandLine line = parser.parse(options, args);
+
+        if (line.hasOption('h')) {
+            io.out.println(getName() + " -- starts a GShell server");
+            io.out.println();
+
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(
+                io.out,
+                80, // width (FIXME: Should pull from gshell.columns variable)
+                getName() + " [options]",
+                "",
+                options,
+                4, // left pad
+                4, // desc pad
+                "",
+                false); // auto usage
+
+            io.out.println();
+
+            return Command.SUCCESS;
+        }
+
+        if (line.hasOption('p')) {
+            String tmp = line.getOptionValue('p');
+            port = Integer.parseInt(tmp);
+        }
+
+        server();
+
+        return Command.SUCCESS;
+    }
+
+    private void server() throws Exception {
+        SocketHandler handler = new SocketHandler() {
+            GShellServer server = new GShellServer();
+
+            public void handle(final Socket socket) throws Exception {
+                assert socket != null;
+
+                server.service(socket);
+            }
+        };
+
+        SocketServerDaemon daemon = new SocketServerDaemon(port, handler);
+
+        IO io = getIO();
+        io.out.println("Listening for connections on port: " + port);
+        io.flush();
+
+        daemon.start();
+    }
+}
---
+++ gshell-server/gshell-server-telnet/src/main/java/org/apache/geronimo/gshell/server/telnet/TelnetTerminal.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.geronimo.gshell.server.telnet;
+
+import nvt4j.impl.Terminal;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+import org.apache.geronimo.gshell.console.IO;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Telnet <a href="http://jline.sf.net">JLine</a> terminal implementation
+ * backed up by a <a href="http://www.bway.net/~lichtner/nvt4j.html">NVT4J</a> terminal instance.
+ *
+ * @version $Id$
+ */
+public class TelnetTerminal
+    extends jline.Terminal
+{
+    private static final Log log = LogFactory.getLog(TelnetTerminal.class);
+
+    private final Terminal term;
+
+    private final IO io;
+
+    public TelnetTerminal(final InputStream input, final OutputStream output) throws IOException {
+        this.term = new Terminal(input, output);
+        this.io =  new IO(createInputStream(), createOutputStream());
+    }
+
+    public void initializeTerminal() throws Exception {
+        //
+        // NVT4j does all init in Terminal.init() which is called from constructor
+        //
+    }
+
+    public int getTerminalWidth() {
+        return term.getColumns();
+    }
+
+    public int getTerminalHeight() {
+        return term.getRows();
+    }
+
+    public boolean isSupported() {
+        return true;
+    }
+
+    public boolean getEcho() {
+        return false;
+    }
+
+    private InputStream createInputStream() {
+        return new InputStream() {
+            public int read() throws IOException {
+                return term.get();
+            }
+        };
+    }
+
+    private OutputStream createOutputStream() {
+        return new OutputStream() {
+            public void write(final int i) throws IOException {
+                term.put(i);
+            }
+        };
+    }
+
+    public IO getIO() {
+        return io;
+    }
+
+    //
+    // NOTE: Copied (and modified) from jline.UnixTerminal
+    //
+
+    public static final short ARROW_START = 27;
+
+    public static final short ARROW_PREFIX = 91;
+
+    public static final short ARROW_LEFT = 68;
+
+    public static final short ARROW_RIGHT = 67;
+
+    public static final short ARROW_UP = 65;
+
+    public static final short ARROW_DOWN = 66;
+
+    public static final short HOME_CODE = 72;
+
+    public static final short END_CODE = 70;
+
+    public int readVirtualKey(final InputStream in) throws IOException {
+        assert in != null;
+
+        int c = readCharacter(in);
+
+        //
+        // TODO: Need to check if this is correct... arrow handling is a tad off
+        //
+
+        // in Unix terminals, arrow keys are represented by
+        // a sequence of 3 characters. E.g., the up arrow
+        // key yields 27, 91, 68
+
+        if (c == ARROW_START) {
+            c = readCharacter(in);
+
+            if (c == ARROW_PREFIX) {
+                c = readCharacter(in);
+
+                switch (c) {
+                    case ARROW_UP:
+                        return CTRL_P;
+
+                    case ARROW_DOWN:
+                        return CTRL_N;
+
+                    case ARROW_LEFT:
+                        return CTRL_B;
+
+                    case ARROW_RIGHT:
+                        return CTRL_F;
+
+                    case HOME_CODE:
+                        return CTRL_A;
+
+                    case END_CODE:
+                        return CTRL_E;
+                }
+            }
+        }
+
+        return c;
+    }
+}
