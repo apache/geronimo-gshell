e35daf9b069f1ece60bc9665ef82795987daa687
Jason Dillon <jdillon@apache.org>
2007-05-06 22:37:10 +0000

    Don't require JAVA_HOME to be set, default to java Strip off version from boot/classworlds.jar to simplify things a wee bit
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/gshell/trunk@535668 13f79535-47bb-0310-9956-ffa450edef68

--- gshell-assembly/src/main/scripts/gsh.bat
+++ gshell-assembly/src/main/scripts/gsh.bat
@@ -25,9 +25,6 @@ rem
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-@rem The version of classworlds to boot with
-set CLASSWORLDS_VERSION=1.1
-
 :begin
 @rem Determine what directory it is in.
 set DIRNAME=%~dp0
@@ -99,7 +96,7 @@ set CMD_LINE_ARGS=%$
 
 :execute
 @rem Setup the command line
-set CLASSWORLDS_CLASSPATH=%GSHELL_HOME%\lib\boot\classworlds-%CLASSWORLDS_VERSION%.jar
+set CLASSWORLDS_CLASSPATH=%GSHELL_HOME%\lib\boot\classworlds.jar
 set CLASSWORLDS_MAIN_CLASS=org.codehaus.classworlds.Launcher
 set CLASSWORLDS_CONF=%GSHELL_HOME%\etc\%PROGNAME%.conf
 
