72cabd45568f6eae6afb1758ff60b5da21eb525e
Jason Dillon <jdillon@apache.org>
2008-02-28 04:39:12 +0000

    (GSHELL-102) Upgrade to Mina 1.1.6
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/gshell/trunk@631843 13f79535-47bb-0310-9956-ffa450edef68

--- gshell-whisper/src/main/java/org/apache/geronimo/gshell/whisper/request/Requestor.java
+++ gshell-whisper/src/main/java/org/apache/geronimo/gshell/whisper/request/Requestor.java
@@ -129,6 +129,10 @@ public class Requestor
             return delegate.getSession();
         }
 
+        public Object getLock() {
+            return delegate.getLock();
+        }
+
         public void join() {
             delegate.join();
         }
