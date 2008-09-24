6993526d6efa19012b9221f85e85ea2b82a80567
Jason Dillon <jdillon@apache.org>
2008-09-24 16:53:26 +0000

    Have the basic functionality working for the spring "gshell" namespace
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/gshell/trunk@698648 13f79535-47bb-0310-9956-ffa450edef68

--- gshell-wisdom/gshell-wisdom-core/src/main/java/org/apache/geronimo/gshell/wisdom/config/PluginParser.java
+++ gshell-wisdom/gshell-wisdom-core/src/main/java/org/apache/geronimo/gshell/wisdom/config/PluginParser.java
@@ -20,13 +20,24 @@
 package org.apache.geronimo.gshell.wisdom.config;
 
 import org.apache.geronimo.gshell.wisdom.plugin.PluginImpl;
+import org.apache.geronimo.gshell.wisdom.plugin.bundle.CommandBundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.ManagedList;
 import org.springframework.beans.factory.xml.AbstractBeanDefinitionParser;
+import org.springframework.beans.factory.xml.BeanDefinitionParserDelegate;
 import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.util.StringUtils;
 import org.springframework.util.xml.DomUtils;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -37,26 +48,284 @@ import java.util.List;
 public class PluginParser
     extends AbstractBeanDefinitionParser
 {
-    protected AbstractBeanDefinition parseInternal(final Element element, final ParserContext parserContext) {
+    @Override
+    protected boolean shouldGenerateId() {
+		return true;
+	}
+
+    @Override
+	protected boolean shouldGenerateIdAsFallback() {
+		return true;
+	}
+
+    protected AbstractBeanDefinition parseInternal(final Element element, final ParserContext context) {
         assert element != null;
-        assert parserContext != null;
+        assert context != null;
 
-        BeanDefinitionBuilder plugin = BeanDefinitionBuilder.rootBeanDefinition(PluginImpl.class);
-        plugin.addPropertyValue("id", element.getAttribute("name"));
-
-        @SuppressWarnings({"unchecked"})
-        List<Element> children = DomUtils.getChildElementsByTagName(element, "command-bundle");
-        if (children != null && children.size() > 0) {
-            parseCommandBundles(plugin, children);
-        }
-
+        Builder builder = new Builder(context);
+        BeanDefinitionBuilder plugin = builder.buildPlugin(element);
         return plugin.getBeanDefinition();
     }
 
-    private void parseCommandBundles(final BeanDefinitionBuilder plugin, final List<Element> elements) {
-        assert elements != null;
-        assert plugin != null;
+    /**
+     * Helper to deal with command type.
+     */
+    private enum CommandType
+    {
+        STATELESS,
+        STATEFUL;
 
+        public static CommandType parse(final String text) {
+            assert text != null;
 
+            return valueOf(text.toUpperCase());
+        }
+
+        public String getTemplateName() {
+            return name().toLowerCase() + "CommandTemplate";
+        }
+
+        public void wire(final BeanDefinitionBuilder command, final BeanDefinitionHolder action) {
+            assert command != null;
+            assert action != null;
+
+            switch (this) {
+                case STATELESS:
+                    command.addPropertyReference("action", action.getBeanName());
+                    break;
+
+                case STATEFUL:
+                    command.addPropertyValue("actionId", action.getBeanName());
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Helper to build plugin related bean definitions.
+     */
+    private class Builder
+    {
+        private final Logger log = LoggerFactory.getLogger(getClass());
+
+        private ParserContext context;
+
+        public Builder(final ParserContext context) {
+            assert context != null;
+
+            this.context = context;
+        }
+
+        private String resolveId(final Element element, final BeanDefinition def) throws BeanDefinitionStoreException {
+            assert element != null;
+            assert def != null;
+
+            if (shouldGenerateId()) {
+                return context.getReaderContext().generateBeanName(def);
+            }
+
+            String id = element.getAttribute(ID_ATTRIBUTE);
+
+            if (!StringUtils.hasText(id) && shouldGenerateIdAsFallback()) {
+                id = context.getReaderContext().generateBeanName(def);
+            }
+
+            return id;
+        }
+
+        @SuppressWarnings({"unchecked"})
+        private List<Element> getChildElements(final Element element, final String name) {
+            assert element != null;
+            assert name != null;
+
+            return DomUtils.getChildElementsByTagName(element, name);
+        }
+
+        @SuppressWarnings({"unchecked"})
+        private Element getChildElement(final Element element, final String name) {
+            assert element != null;
+            assert name != null;
+
+            List<Element> elements = DomUtils.getChildElementsByTagName(element, name);
+            if (elements != null && !elements.isEmpty()) {
+                return elements.get(0);
+            }
+            return null;
+        }
+
+        private BeanDefinitionHolder parseBeanDefinitionElement(final Element element) {
+            assert element != null;
+
+            BeanDefinitionParserDelegate parser = new BeanDefinitionParserDelegate(context.getReaderContext());
+            parser.initDefaults(element.getOwnerDocument().getDocumentElement());
+            return parser.parseBeanDefinitionElement(element);
+        }
+
+        private BeanDefinitionHolder register(final BeanDefinitionHolder holder) {
+            assert holder != null;
+
+            registerBeanDefinition(holder, context.getRegistry());
+            return holder;
+        }
+
+        private BeanDefinitionHolder register(final BeanDefinition def, final String id) {
+            assert def != null;
+            assert id != null;
+
+            BeanDefinitionHolder holder = new BeanDefinitionHolder(def, id);
+            return register(holder);
+        }
+
+        public BeanDefinitionBuilder buildPlugin(final Element element) {
+            assert element != null;
+
+            BeanDefinitionBuilder plugin = parsePlugin(element);
+
+            List<BeanDefinitionHolder> bundles = parseCommandBundles(element);
+
+            for (BeanDefinitionHolder holder : bundles) {
+                // TODO: Handle registration of the bundles?
+            }
+
+            return plugin;
+        }
+
+        private BeanDefinitionBuilder parsePlugin(final Element element) {
+            assert element != null;
+
+            log.info("Parse plugin; element: {}", element);
+
+            BeanDefinitionBuilder plugin = BeanDefinitionBuilder.rootBeanDefinition(PluginImpl.class);
+            plugin.addPropertyValue("id", element.getAttribute("name"));
+
+            return plugin;
+        }
+
+        private List<BeanDefinitionHolder> parseCommandBundles(final Element element) {
+            assert element != null;
+
+            log.info("Parse command bundles; element: {}", element);
+
+            List<Element> children = getChildElements(element, "command-bundle");
+            List<BeanDefinitionHolder> holders = new ArrayList<BeanDefinitionHolder>();
+
+            for (Element child : children) {
+                BeanDefinitionBuilder bundle = parseCommandBundle(child);
+
+                // Generate id and register the bean
+                AbstractBeanDefinition def = bundle.getBeanDefinition();
+                String id = resolveId(child, def);
+                BeanDefinitionHolder holder = register(def, id);
+
+                // noinspection unchecked
+                holders.add(holder);
+            }
+
+            return holders;
+        }
+
+        private BeanDefinitionBuilder parseCommandBundle(final Element element) {
+            assert element != null;
+
+            log.info("Parse command bundle; element; {}", element);
+
+            BeanDefinitionBuilder bundle = BeanDefinitionBuilder.rootBeanDefinition(CommandBundle.class);
+            bundle.addPropertyValue("id", element.getAttribute("name"));
+            bundle.setLazyInit(true);
+
+            List commands = parseCommands(element);
+            bundle.addPropertyValue("commands", commands);
+            
+            return bundle;
+        }
+
+        private List parseCommands(final Element element) {
+            assert element != null;
+
+            log.info("Parse commands; element; {}", element);
+
+            List<Element> children = getChildElements(element, "command");
+            ManagedList defs = new ManagedList();
+
+            for (Element child : children) {
+                BeanDefinitionBuilder command = parseCommand(child);
+
+                // noinspection unchecked
+                defs.add(command.getBeanDefinition());
+            }
+
+            return defs;
+        }
+
+        private BeanDefinitionBuilder parseCommand(final Element element) {
+            assert element != null;
+
+            log.info("Parse command; element; {}", element);
+
+            CommandType type = CommandType.parse(element.getAttribute("type"));
+            BeanDefinitionBuilder command = BeanDefinitionBuilder.childBeanDefinition(type.getTemplateName());
+
+            // TODO: Currently name is pulled from the documentor, need to change that
+            // command.addPropertyValue("name", element.getAttribute("name"));
+
+            BeanDefinitionHolder action = parseCommandAction(element);
+
+            // Wire up the action based on the type
+            type.wire(command, action);
+
+            return command;
+        }
+
+        private BeanDefinitionHolder parseCommandAction(final Element element) {
+            assert element != null;
+
+            log.info("Parse command action; element; {}", element);
+
+            Attr actionAttr = element.getAttributeNode("action");
+            Element actionElement = getChildElement(element, "action");
+
+            // Validate that we only have one action
+            if (actionAttr != null && actionElement != null) {
+                throw new RuntimeException("Must specify only one action attribute or action element");
+            }
+            if (actionAttr == null && actionElement == null) {
+                throw new RuntimeException("Missing action attribute or action element");
+            }
+
+            // Construct the action
+            BeanDefinition action;
+
+            if (actionAttr != null) {
+                action = BeanDefinitionBuilder.rootBeanDefinition(actionAttr.getValue()).getBeanDefinition();
+            }
+            else {
+                // TODO: Can probably just treat the "action" element as a "bean" element?
+                
+                Attr classAttr = actionElement.getAttributeNode("class");
+                Element beanElement = getChildElement(actionElement, "bean");
+
+                // Validate we only have one configuration for the action
+                if (classAttr != null && beanElement != null) {
+                    throw new RuntimeException("Must specify only one class attribute or bean element");
+                }
+                if (classAttr == null && beanElement == null) {
+                    throw new RuntimeException("Missing action class attribute or action bean element");
+                }
+
+                if (classAttr != null) {
+                    action = BeanDefinitionBuilder.rootBeanDefinition(classAttr.getValue()).getBeanDefinition();
+                }
+                else {
+                    action = parseBeanDefinitionElement(beanElement).getBeanDefinition();
+                }
+            }
+
+            // All actions are configured as prototypes
+            action.setScope("prototype");
+
+            // Generate id and register the bean
+            String id = resolveId(element, action);
+            return register(action, id);
+        }
     }
 }
---
+++ gshell-wisdom/gshell-wisdom-core/src/test/java/org/apache/geronimo/gshell/wisdom/config/PluginParserTest.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.geronimo.gshell.wisdom.config;
+
+import org.apache.geronimo.gshell.spring.LoggingProcessor;
+import org.apache.geronimo.gshell.spring.SpringTestSupport;
+
+/**
+ * Unit tests for the {@link LoggingProcessor} class.
+ *
+ * @version $Rev: 698145 $ $Date: 2008-09-23 18:39:31 +0700 (Tue, 23 Sep 2008) $
+ */
+public class PluginParserTest
+    extends SpringTestSupport
+{
+    public void testParser() throws Exception {
+        LoggingProcessor processor = new LoggingProcessor();
+        String xml = processor.render(applicationContext.getBeanFactory());
+        assertNotNull(xml);
+
+        System.out.println("XML:\n" + xml);
+    }
+}
