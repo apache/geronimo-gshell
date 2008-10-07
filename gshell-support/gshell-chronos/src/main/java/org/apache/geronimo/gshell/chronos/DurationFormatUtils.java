b74e04802108b99437cf815229c308d0a42c552e
Jason Dillon <jdillon@apache.org>
2008-10-07 09:04:59 +0000

    Unmark TODO so IDEA won't keep bugging me about these, not going to do anything about it
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/gshell/trunk@702400 13f79535-47bb-0310-9956-ffa450edef68

--- gshell-support/gshell-chronos/src/main/java/org/apache/geronimo/gshell/chronos/DurationFormatUtils.java
+++ gshell-support/gshell-chronos/src/main/java/org/apache/geronimo/gshell/chronos/DurationFormatUtils.java
@@ -283,7 +283,7 @@ public class DurationFormatUtils
         // Used to optimise for differences under 28 days and
         // called formatDuration(millis, format); however this did not work
         // over leap years.
-        // TODO: Compare performance to see if anything was lost by
+        // T O D O: Compare performance to see if anything was lost by
         // losing this optimisation.
 
         Token[] tokens = lexx(format);
@@ -509,7 +509,7 @@ public class DurationFormatUtils
             }
             Object value = null;
             switch(ch) {
-                // TODO: Need to handle escaping of '
+                // T O D O: Need to handle escaping of '
                 case '\'' :
                   if(inLiteral) {
                       buffer = null;
