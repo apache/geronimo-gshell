75d45b30fb1717c68af280eb4dd180ee5ad2edcb
Jason Dillon <jdillon@apache.org>
2008-10-20 08:19:02 +0000

    Completely ripped out ClassWorlds, using a smaller/faster custom Launcher
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/gshell/trunk@706145 13f79535-47bb-0310-9956-ffa450edef68

---
+++ gshell-bootstrap/src/main/java/org/apache/geronimo/gshell/bootstrap/Configuration.java
@@ -0,0 +1,168 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.geronimo.gshell.bootstrap;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileFilter;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Bootstrap configuration.
+ *
+ * @version $Rev$ $Date$
+ */
+public class Configuration
+{
+    private static final String GSHELL_HOME = "gshell.home";
+
+    private static final String PROGRAM_NAME = "program.name";
+
+    private static final String DEFAULT_PROGRAM_NAME = "gsh";
+
+    private static final String LOG4J_CONF = "log4j.configuration";
+
+    private static final String DEFAULT_LOG4J_CONF = "org/apache/geronimo/gshell/bootstrap/default-log4j.xml";
+
+    private File homeDir;
+
+    private String programName;
+
+    private String log4jConfig;
+
+    private ClassLoader classLoader;
+
+    public File getHomeDir() throws IOException {
+        if (homeDir == null) {
+            String path = System.getProperty(GSHELL_HOME);
+            File dir;
+
+            if (path == null) {
+                String jarPath = Launcher.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+                jarPath = java.net.URLDecoder.decode(jarPath, "UTF-8");
+
+                // The jar containing this class is expected to be in <gshell.home>/lib/boot
+                File bootJar = new File(jarPath);
+                dir = bootJar.getParentFile().getParentFile().getParentFile().getCanonicalFile();
+            }
+            else {
+                dir = new File(path).getCanonicalFile();
+            }
+
+            homeDir = dir;
+        }
+
+        return homeDir;
+    }
+
+    public String getProgramName() {
+        if (programName == null) {
+            programName = System.getProperty(PROGRAM_NAME, DEFAULT_PROGRAM_NAME);
+        }
+
+        return programName;
+    }
+
+    public File getProgramConfigurationFile(final String filename) throws IOException {
+        assert filename != null;
+
+        File etcDir = new File(getHomeDir(), "etc");
+        File file = new File(etcDir, getProgramName() + "-" + filename);
+
+        if (!file.exists()) {
+            File unprefixedFile = new File(etcDir, filename);
+
+            if (unprefixedFile.exists()) {
+                file = unprefixedFile;
+            }
+        }
+
+        return file;
+    }
+
+    public String getLog4jConfig() throws IOException {
+        if (log4jConfig == null) {
+            String path = System.getProperty(LOG4J_CONF);
+            File file;
+
+            if (path == null) {
+                file = getProgramConfigurationFile("log4j.xml");
+            }
+            else {
+                file = new File(path).getCanonicalFile();
+            }
+
+            if (file.exists()) {
+                log4jConfig = file.toURI().toURL().toExternalForm();
+            }
+            else {
+                log4jConfig = DEFAULT_LOG4J_CONF;
+            }
+        }
+
+        return log4jConfig;
+    }
+
+    private void setProperty(final String name, final String value) {
+        System.setProperty(name, value);
+        Log.debug("Property: " + name + "=" + value);
+    }
+
+    public void configure() throws Exception {
+        setProperty(PROGRAM_NAME, getProgramName());
+        setProperty(GSHELL_HOME, getHomeDir().getAbsolutePath());
+        setProperty(LOG4J_CONF, getLog4jConfig());
+    }
+
+    public ClassLoader getClassLoader() throws Exception {
+        if (classLoader == null) {
+            List<URL> classPath = new ArrayList<URL>();
+
+            // Add ${gshell.home}/etc
+            classPath.add(new File(getHomeDir(), "etc").toURI().toURL());
+
+            // Add ${gshell.home}/lib/*.jar
+            File libDir = new File(getHomeDir(), "lib");
+            File[] files = libDir.listFiles(new FileFilter() {
+                public boolean accept(final File file) {
+                    return file.isFile();
+                }
+            });
+
+            for (File file : files) {
+                classPath.add(file.toURI().toURL());
+            }
+
+            if (Log.DEBUG) {
+                Log.debug("Classpath:");
+                for (URL url : classPath) {
+                    Log.debug("    " + url);
+                }
+            }
+
+            classLoader = new URLClassLoader(classPath.toArray(new URL[classPath.size()]), getClass().getClassLoader());
+        }
+
+        return classLoader;
+    }
+}
--- gshell-bootstrap/src/main/java/org/apache/geronimo/gshell/bootstrap/Launcher.java
+++ gshell-bootstrap/src/main/java/org/apache/geronimo/gshell/bootstrap/Launcher.java
@@ -19,232 +19,66 @@
 
 package org.apache.geronimo.gshell.bootstrap;
 
-import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URL;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 /**
- * Platform independent launcher to setup common configuration and launch the application.
+ * Bootstrap launcher.
  *
  * @version $Rev$ $Date$
  */
 public class Launcher
-    extends org.codehaus.plexus.classworlds.launcher.Launcher
 {
-    private static final String PROGRAM_NAME = "program.name";
+    private static final int SUCCESS_EXIT_CODE = 0;
 
-    private static final String GSHELL_HOME = "gshell.home";
+    private static final int FAILURE_EXIT_CODE = 100;
 
-    private static final String LOG4J_CONF = "log4j.configuration";
-
-    private static final String DEFAULT_LOG4J_CONF = "org/apache/geronimo/gshell/bootstrap/log4j.xml";
-
-    private static final String DEFAULT_CLASSWORLDS_CONF = "org/apache/geronimo/gshell/bootstrap/classworlds.conf";
-
-    private static boolean debug = Boolean.getBoolean(Launcher.class.getName() + ".debug");
-
-    private static String programName;
-    
-    private static File homeDir;
-
-    private static void debug(final String message) {
-        if (debug) {
-            System.err.println("[DEBUG] " + message);
-        }
-    }
-
-    private static void warn(final String message) {
-        System.err.println("[WARN] " + message);
-    }
-
-    private static void error(final String message) {
-        System.err.println("[ERROR] " + message);
-        throw new Error(message);
-    }
-
-    private static void setProperty(final String name, final String value) {
-        System.setProperty(name, value);
-        debug("Setting property: " + name + "=" + value);
-    }
-
-    private static String getProgramName() {
-        String name = System.getProperty(PROGRAM_NAME);
-        if (name == null) {
-            name = "gsh";
-        }
-
-        return name;
-    }
-
-    private static File getHomeDir() throws Exception {
-        String path = System.getProperty(GSHELL_HOME);
-        File dir;
-
-        if (path == null) {
-            String jarPath = Launcher.class.getProtectionDomain().getCodeSource().getLocation().getFile();
-            jarPath = java.net.URLDecoder.decode(jarPath, "UTF-8");
-
-            // The jar containing this class is expected to be in <gshell.home>/lib/boot
-            File bootJar = new File(jarPath);
-            dir = bootJar.getParentFile().getParentFile().getParentFile().getCanonicalFile();
-        }
-        else {
-            dir = new File(path).getCanonicalFile();
-        }
-
-        return dir;
-    }
-
-    private static File getProgramConfigurationFile(final String filename) {
-        assert filename != null;
-
-        File etcDir = new File(homeDir, "etc");
-
-        File file = new File(etcDir, programName + "-" + filename);
-
-        if (!file.exists()) {
-            File unprefixedFile = new File(etcDir, filename);
-            
-            if (unprefixedFile.exists()) {
-                file = unprefixedFile;
-            }
-        }
-
-        return file;
-    }
-
-    private static String getClassworldsConf() throws Exception {
-        String path = System.getProperty(CLASSWORLDS_CONF);
-        File file;
-
-        if (path == null) {
-            file = getProgramConfigurationFile("classworlds.conf");
-        }
-        else {
-            file = new File(path).getCanonicalFile();
-        }
-
-        if (file.exists()) {
-            return file.getCanonicalPath();
-        }
-
-        return DEFAULT_CLASSWORLDS_CONF;
-    }
-
-    private static String getLog4jConf() throws Exception {
-        String path = System.getProperty(LOG4J_CONF);
-        File file;
-
-        if (path == null) {
-            file = getProgramConfigurationFile("log4j.xml");
-        }
-        else {
-            file = new File(path).getCanonicalFile();
-        }
-
-        if (file.exists()) {
-            return file.toURI().toURL().toExternalForm();
-        }
-
-        return DEFAULT_LOG4J_CONF;
-    }
-
-    private static void configure() throws Exception {
-        // Branding information is not available here, so we must use the basic GShell properties to configure the bootstrap loader.
-
-        programName = getProgramName();
-        setProperty(PROGRAM_NAME, programName);
-
-        homeDir = getHomeDir();
-        setProperty(GSHELL_HOME, homeDir.getCanonicalPath());
-
-        String classworldsConf = getClassworldsConf();
-        if (classworldsConf != null) {
-            setProperty(CLASSWORLDS_CONF, classworldsConf);
-        }
-
-        String log4jConf = getLog4jConf();
-        if (log4jConf != null) {
-            setProperty(LOG4J_CONF, log4jConf);
-        }
-        else {
-            warn("Missing Log4j configuration! Logging may not properly initialize;  Please set the '" +
-                    LOG4J_CONF + "' property to a valid configuration file or resource name.");
-        }
-    }
-
-    public static int mainWithExitCode(final String[] args) throws Exception {
-        assert args != null;
-
-        Launcher launcher = new Launcher();
-
-        ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        launcher.setSystemClassLoader(cl);
-
-        String classworldsConf = System.getProperty(CLASSWORLDS_CONF);
-        URL configUrl = null;
-
-        if (classworldsConf != null) {
-            File file = new File(classworldsConf);
-            if (file.exists()) {
-                configUrl = file.toURI().toURL();
-            }
-            else {
-                configUrl = cl.getResource(classworldsConf);
-            }
-        }
-
-        if (configUrl == null) {
-            error("Failed to locate ClassWorlds configuration!  Please set the '" +
-                    CLASSWORLDS_CONF + "' property to a valid configuration file or resource name.");
-            // Unreachable statement, ^^^ throws Error w/message
-            throw new Error();
-        }
-
-        debug("Resolved " + CLASSWORLDS_CONF + ": " + configUrl);
-
-        launcher.configure(configUrl.openStream());
-
-        try {
-            launcher.launch(args);
-        }
-        catch (InvocationTargetException e) {
-            throw new LaunchFailedError(e.getTargetException());
-        }
-
-        return launcher.getExitCode();
-    }
+    private static final String MAIN_CLASS = "org.apache.geronimo.gshell.cli.Main";
 
     public static void main(final String[] args) {
         assert args != null;
 
+        Configuration config = new Configuration();
+
         try {
-            configure();
+            Log.debug("Configuring");
+            
+            config.configure();
 
-            int exitCode = mainWithExitCode(args);
+            ClassLoader cl = config.getClassLoader();
+            Class type = cl.loadClass(MAIN_CLASS);
+            Method method = getMainMethod(type);
 
-            System.exit(exitCode);
+            Thread.currentThread().setContextClassLoader(cl);
+
+            Log.debug("Launching: " + method);
+
+            method.invoke(null, new Object[] { args });
+
+            Log.debug("Exiting");
+
+            System.exit(SUCCESS_EXIT_CODE);
         }
         catch (Throwable t) {
             t.printStackTrace(System.err);
             System.err.flush();
-            System.exit(100);
+            System.exit(FAILURE_EXIT_CODE);
         }
     }
 
-    //
-    // LaunchFailedError
-    //
-    
-    private static class LaunchFailedError
-        extends Error
-    {
-        public LaunchFailedError(final Throwable cause) {
-            super(cause);
+    private static Method getMainMethod(final Class type) throws Exception {
+        assert type != null;
+
+        Method m = type.getMethod("main", String[].class);
+
+        int modifiers = m.getModifiers();
+
+        if (Modifier.isStatic(modifiers) && Modifier.isPublic(modifiers)) {
+            if (m.getReturnType() == Integer.TYPE || m.getReturnType() == Void.TYPE) {
+                return m;
+            }
         }
 
-        public LaunchFailedError(final String msg) {
-            super(msg);
-        }
+        throw new NoSuchMethodException("public static void main(String[] args) in " + type);
     }
 }
