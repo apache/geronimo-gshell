4af8aeaf4a421e0860195f00cf0ad9f76f83671a
Jason Dillon <jdillon@apache.org>
2008-11-25 04:56:23 +0000

    Add JSecurity-based password auth
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/gshell/trunk@720415 13f79535-47bb-0310-9956-ffa450edef68

--- gshell-commands/gshell-ssh/src/main/java/org/apache/geronimo/gshell/commands/ssh/JSecurityPasswordAuthenticator.java
+++ gshell-commands/gshell-ssh/src/main/java/org/apache/geronimo/gshell/commands/ssh/JSecurityPasswordAuthenticator.java
@@ -20,16 +20,42 @@
 package org.apache.geronimo.gshell.commands.ssh;
 
 import com.google.code.sshd.server.PasswordAuthenticator;
+import org.jsecurity.SecurityUtils;
+import org.jsecurity.authc.AuthenticationException;
+import org.jsecurity.authc.UsernamePasswordToken;
+import org.jsecurity.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Bogus {@link PasswordAuthenticator}. 
+ * <a href="http://jsecurity.org">JSecurity</a> {@link PasswordAuthenticator}.
  *
  * @version $Rev$ $Date$
  */
-public class BogusPasswordAuthenticator
+public class JSecurityPasswordAuthenticator
     implements PasswordAuthenticator
 {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     public Object authenticate(final String username, final String password) {
-        return (username != null && username.equals(password)) ? username : null;
+        assert username != null;
+        assert password != null;
+        
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+
+            try {
+                currentUser.login(token);
+                log.info("User [" + currentUser.getPrincipal() + "] logged in successfully");
+            }
+            catch (AuthenticationException e) {
+                log.error("Authentication failed: " + e, e);
+                return null;
+            }
+        }
+
+        return currentUser.getPrincipal();
     }
 }
