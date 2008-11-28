beebcc0b491e33918d992d3d8cc2a98b1ad5cbae
Jason Dillon <jdillon@apache.org>
2008-11-28 11:25:42 +0000

    Split up gshell-opitional into linux/rpm-like packages/plugins
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/gshell/trunk@721437 13f79535-47bb-0310-9956-ffa450edef68

---
+++ gshell-commands/gshell-shellutils/src/main/java/org/apache/geronimo/gshell/commands/shellutils/SleepAction.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.geronimo.gshell.commands.shellutils;
+
+import org.apache.geronimo.gshell.clp.Argument;
+import org.apache.geronimo.gshell.command.CommandAction;
+import org.apache.geronimo.gshell.command.CommandContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Sleep... zzzZ
+ *
+ * @version $Rev$ $Date$
+ */
+public class SleepAction
+    implements CommandAction
+{
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
+    @Argument(required=true)
+    private int time = -1;
+
+    public Object execute(final CommandContext context) throws Exception {
+        assert context != null;
+
+        log.info("Sleeping for {}", time);
+
+        try {
+            Thread.sleep(time);
+        }
+        catch (InterruptedException ignore) {
+            log.debug("Sleep was interrupted... :-(");
+        }
+
+        log.info("Awake now");
+
+        return Result.SUCCESS;
+    }
+}
