b2344923a4b032344271bf07911c3f2bf9459daf
Jason Dillon <jdillon@apache.org>
2008-11-30 07:46:21 +0000

    Add a simple download monitor
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/gshell/trunk@721779 13f79535-47bb-0310-9956-ffa450edef68

--- gshell-support/gshell-artifact/src/main/java/org/apache/geronimo/gshell/artifact/transfer/monitor/SimpleMonitor.java
+++ gshell-support/gshell-artifact/src/main/java/org/apache/geronimo/gshell/artifact/transfer/monitor/SimpleMonitor.java
@@ -26,33 +26,19 @@ import org.apache.geronimo.gshell.io.IO;
 import java.io.IOException;
 
 /**
- * A download monitor providing a simple spinning progress interface.
+ * A simple download monitor.
  *
  * @version $Rev$ $Date$
  */
-public class ProgressSpinnerMonitor
+public class SimpleMonitor
     extends TransferListenerSupport
 {
-    private static final String CARRIAGE_RETURN = "\r";
-
     private final IO io;
 
-    private final ProgressSpinner spinner = new ProgressSpinner();
-
-    private long complete;
-
-    public ProgressSpinnerMonitor(final IO io) throws IOException {
+    public SimpleMonitor(final IO io) throws IOException {
         assert io != null;
 
         this.io = io;
-    }
-    
-    private void print(final String message) {
-        if (!io.isQuiet()) {
-            io.out.print(message);
-            io.out.print(CARRIAGE_RETURN);
-            io.out.flush();
-        }
     }
 
     private void println(final String message) {
@@ -67,37 +53,14 @@ public class ProgressSpinnerMonitor
 
         super.transferStarted(event);
 
-        complete = 0;
-
-        spinner.reset();
-
         String type = event.getRequestType() == TransferEvent.RequestType.UPLOAD ? "Uploading" : "Downloading";
         String location = event.getLocation();
 
         String message = type + ": " + location;
 
+        log.debug(message);
+
         println(message);
-    }
-
-    public void transferProgress(final TransferEvent event) {
-        assert event != null;
-
-        super.transferProgress(event);
-
-        long total = event.getContentLength();
-        complete += event.getLength();
-
-        String message;
-
-        if (total >= 1024) {
-            message = complete / 1024 + "/" + (total == TransferEvent.UNKNOWN_LENGTH ? "?" : total / 1024 + "K");
-        }
-        else {
-            message = complete + "/" + (total == TransferEvent.UNKNOWN_LENGTH ? "?" : total + "b");
-        }
-
-        
-        print(spinner.spin(message));
     }
 
     public void transferCompleted(final TransferEvent event) {
@@ -109,7 +72,6 @@ public class ProgressSpinnerMonitor
         String type = event.getRequestType() == TransferEvent.RequestType.UPLOAD ? "Uploaded" : "Downloaded";
         String bytes = total >= 1024 ? ( total / 1024 ) + "K" : total + "b";
 
-        // HACK: pad at end just incase, should really blank the reset of the line
-        print(type + " " + bytes + "          ");
+        println(type + " " + bytes);
     }
 }
